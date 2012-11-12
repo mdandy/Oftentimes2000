523f97749b4a0963fffc165ae2a4b2d2eabb58fc
Jonathan Edwards <jedwards163@gmail.com>
2012-11-12 09:33:26 -0500

    Added SQL Table

---
+++ app/server/Advertisements.sql
@@ -0,0 +1,40 @@
+-- phpMyAdmin SQL Dump
+-- version 3.5.1
+-- http://www.phpmyadmin.net
+--
+-- Host: db.cip.gatech.edu
+-- Generation Time: Nov 12, 2012 at 09:31 AM
+-- Server version: 5.5.15-log
+-- PHP Version: 5.3.13
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `CONTRIB_maven`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Advertisements`
+--
+
+CREATE TABLE IF NOT EXISTS `Advertisements` (
+  `name` varchar(100) NOT NULL,
+  `description` varchar(500) NOT NULL,
+  `latitude` double NOT NULL,
+  `longitude` double NOT NULL,
+  `start_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `end_time` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
