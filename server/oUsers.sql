300b7c8ea1d35af42edb3b737b1dd1e8ebecc9ed
Jonathan Edwards <jedwards163@gmail.com>
2012-11-19 11:09:02 -0500

    Added SQL table files, updated UI

--- server/oUsers.sql
+++ server/oUsers.sql
@@ -23,32 +23,21 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `Advertisements`
+-- Table structure for table `oUsers`
 --
 
-CREATE TABLE IF NOT EXISTS `Advertisements` (
+CREATE TABLE IF NOT EXISTS `oUsers` (
   `username` varchar(100) NOT NULL,
-  `name` varchar(100) NOT NULL,
-  `email` varchar(100) NOT NULL,
-  `description` varchar(500) NOT NULL,
-  `latitude` double NOT NULL,
-  `longitude` double NOT NULL,
-  `date` date NOT NULL,
-  `start_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  `end_time` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
-  `category` varchar(300) NOT NULL,
-  KEY `username` (`username`)
+  `email` varchar(300) NOT NULL,
+  `password` varchar(100) NOT NULL,
+  `name` varchar(200) NOT NULL,
+  `street_address` varchar(200) NOT NULL,
+  `state` varchar(100) NOT NULL,
+  `zip_code` int(11) NOT NULL,
+  `website` varchar(200) NOT NULL,
+  `about` varchar(500) NOT NULL,
+  PRIMARY KEY (`username`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Constraints for dumped tables
---
-
---
--- Constraints for table `Advertisements`
---
-ALTER TABLE `Advertisements`
-  ADD CONSTRAINT `Advertisements_ibfk_2` FOREIGN KEY (`username`) REFERENCES `oUsers` (`username`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
