9f2ce9e0e8fba9f5b4023308d7b0661fd1471af8
Michael Dandy <mdandy1988@gmail.com>
2012-11-23 09:20:47 -0500

    Database Schema

---
+++ server/server/CONTRIB_maven.sql
@@ -0,0 +1,74 @@
+-- phpMyAdmin SQL Dump
+-- version 3.5.1
+-- http://www.phpmyadmin.net
+--
+-- Host: db.cip.gatech.edu
+-- Generation Time: Nov 23, 2012 at 09:18 AM
+-- Server version: 5.5.15-log
+-- PHP Version: 5.3.13
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `CONTRIB_maven`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Advertisements`
+--
+
+CREATE TABLE IF NOT EXISTS `Advertisements` (
+  `username` varchar(100) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `description` varchar(500) NOT NULL,
+  `latitude` double NOT NULL,
+  `longitude` double NOT NULL,
+  `date` date NOT NULL,
+  `start_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `end_time` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `category` varchar(300) NOT NULL,
+  KEY `username` (`username`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `oUsers`
+--
+
+CREATE TABLE IF NOT EXISTS `oUsers` (
+  `username` varchar(100) NOT NULL,
+  `email` varchar(300) NOT NULL,
+  `password` varchar(100) NOT NULL,
+  `name` varchar(200) NOT NULL,
+  `street_address` varchar(200) NOT NULL,
+  `state` varchar(100) NOT NULL,
+  `zip_code` int(11) NOT NULL,
+  `website` varchar(200) NOT NULL,
+  `about` varchar(500) NOT NULL,
+  PRIMARY KEY (`username`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `Advertisements`
+--
+ALTER TABLE `Advertisements`
+  ADD CONSTRAINT `Advertisements_ibfk_2` FOREIGN KEY (`username`) REFERENCES `oUsers` (`username`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
