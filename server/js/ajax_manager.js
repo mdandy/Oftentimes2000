002010e06a1b25732ff083b1cced279fe3615d29
Michael Dandy <mdandy1988@gmail.com>
2012-11-23 19:48:36 -0500

    Your account

--- server/js/ajax_manager.js
+++ server/js/ajax_manager.js
@@ -110,5 +110,167 @@ var AJAXManager =
 				console.error(textStatus);
 			}
 		});
+	},
+	
+	update_account: function()
+	{
+		var form = document.forms["account_form"];
+		var username = sessionStorage.username;
+		var password = form.password.value;
+		var password2 = form.password2.value;
+		
+		if (password !== password2)
+		{
+			var template = "<div class='alert alert-error'>";
+			template += "<button type='button' class='close' data-dismiss='alert'>×</button>";
+			template += "<strong>Error!</strong> Password does not match.</div>";
+			$("#account_alert").append(template);
+			return;
+		}
+		
+		if (password == "")
+		{
+			var template = "<div class='alert alert-error'>";
+			template += "<button type='button' class='close' data-dismiss='alert'>×</button>";
+			template += "<strong>Error!</strong> Username and/ or password cannot be empty.</div>";
+			$("#account_alert").append(template);
+			return;
+		}
+	
+		$.ajaxSetup (
+		{
+			cache: false
+		});
+
+		var loadUrl = AJAXManager.url + "user/account";
+		var query = { username: username, 
+					  password: password };
+
+		$.ajax ({
+			type: "POST",
+			url: loadUrl,
+			data: query,
+			dataType: "json",
+			timeout: 5000, //5 seconds
+			success: function(data) 
+			{
+				if (data.res == "TRUE")
+				{
+					var template = "<div class='alert alert-success'>";
+					template += "<button type='button' class='close' data-dismiss='alert'>×</button>";
+					template += "<strong>Success!</strong> Your password has been changed.</div>";
+					$("#account_alert").append(template);
+				}
+				else
+				{
+					var template = "<div class='alert alert-error'>";
+					template += "<button type='button' class='close' data-dismiss='alert'>×</button>";
+					template += "<strong>Error!</strong> Unable to change your password.</div>";
+					$("#account_alert").append(template);
+				}
+			},
+			error: function(jqXHR, textStatus, errorThrown) 
+			{
+				console.error(textStatus);
+			}
+		});
+	},
+	
+	get_profile: function (callback)
+	{
+		var username = sessionStorage.username;
+		
+		$.ajaxSetup (
+		{
+			cache: true
+		});
+
+		var loadUrl = AJAXManager.url + "user/" + username;
+	
+		$.ajax ({
+			type: "GET",
+			url: loadUrl,
+			dataType: "json",
+			timeout: 5000, //5 seconds
+			success: function(data) 
+			{
+				if (data.res == "TRUE")
+				{
+					callback(data.data);
+				}
+			},
+			error: function(jqXHR, textStatus, errorThrown) 
+			{
+				console.error(textStatus);
+			}
+		});
+	},
+	
+	update_profile: function()
+	{
+		var form = document.forms["profile_form"];
+		var username = sessionStorage.username;
+		var name = form.name.value;
+		var street_address = form.street_address.value;
+		var city = form.city.value;
+		var state = form.state.value;
+		var zipcode = form.zipcode.value;
+		var website = form.website.value;
+		var email = form.email.value;
+		var about = form.about.value;
+			
+		if (name == "" || street_address == "" || city == "" || state == "" || zipcode == "" || email == "" || about == "")
+		{
+			var template = "<div class='alert alert-error'>";
+			template += "<button type='button' class='close' data-dismiss='alert'>×</button>";
+			template += "<strong>Error!</strong> Required fields cannot be empty.</div>";
+			$("#profile_alert").append(template);
+			return;
+		}
+	
+		$.ajaxSetup (
+		{
+			cache: false
+		});
+
+		var loadUrl = AJAXManager.url + "user/profile";
+		var query = { username: username, 
+					  name: name, 
+					  street_address: street_address, 
+					  city: city, 
+					  state: state, 
+					  zipcode: zipcode, 
+					  website: website, 
+					  email: email, 
+					  about: about };
+
+		$.ajax ({
+			type: "POST",
+			url: loadUrl,
+			data: query,
+			dataType: "json",
+			timeout: 5000, //5 seconds
+			success: function(data) 
+			{
+				if (data.res == "TRUE")
+				{
+					var template = "<div class='alert alert-success'>";
+					template += "<button type='button' class='close' data-dismiss='alert'>×</button>";
+					template += "<strong>Success!</strong> Your profile has been updated.</div>";
+					$("#profile_alert").append(template);
+				}
+				else
+				{
+					var template = "<div class='alert alert-error'>";
+					template += "<button type='button' class='close' data-dismiss='alert'>×</button>";
+					template += "<strong>Error!</strong> Unable to update your profile.</div>";
+					$("#profile_alert").append(template);
+				}
+			},
+			error: function(jqXHR, textStatus, errorThrown) 
+			{
+				console.error(textStatus);
+			}
+		});
 	}
 };
--- server/js/script.js
+++ server/js/script.js
@@ -1,11 +1,5 @@
 // JavaScript Document
 
-function init_main_page()
-{
-	check_authentication();
-	$("#active_user").html(sessionStorage.username);
-}
-
 function check_authentication()
 {
 	if (!sessionStorage.username)
@@ -19,6 +13,30 @@ function logout()
 {
 	sessionStorage.username = "";
 	window.location.href = "login.html";
+}
+
+function init_main_page()
+{
+	check_authentication();
+	$("#active_user").html(sessionStorage.username);
+}
+
+function init_account_page()
+{
+	AJAXManager.get_profile(init_profile);
+}
+
+function init_profile(profile)
+{
+	var form = document.forms["profile_form"];
+	form.name.value = profile.name;
+	form.street_address.value = profile.street_address;
+	form.city.value = profile.city;
+	form.state.value = profile.state;
+	form.zipcode.value = profile.zipcode;
+	form.website.value = profile.website;
+	form.email.value = profile.email;
+	form.about.value = profile.about;
 }
 
 function format_announcement_form(index) 
