9123cd45a5652ede0ef05e92049ffe6c26e0e21b
Michael Dandy <mdandy1988@gmail.com>
2012-11-25 00:22:06 -0500

    Parcelate all the things

--- app/src/edu/gatech/oftentimes2000/data/Address.java
+++ app/src/edu/gatech/oftentimes2000/data/Address.java
@@ -1,43 +1,78 @@
 package edu.gatech.oftentimes2000.data;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 import com.google.android.maps.GeoPoint;
 
-public class Address 
+public class Address implements Parcelable
 {
 	/**
 	 * The street
 	 */
-	public String street = "";
+	public String street;
 	
 	/**
 	 * The city
 	 */
-	public String city = "";
+	public String city;
 	
 	/**
 	 * The state
 	 */
-	public String state = "";
+	public String state;
 	
 	/**
 	 * The zip code
 	 */
-	public String zipcode = "";
+	public String zipcode;
 	
 	/**
 	 * The phone number
 	 */
-	public String phoneNumber = "";
+	public String phoneNumber;
 	
 	/**
 	 * The radius
 	 */
-	public int radius = 0;
+	public int radius;
 	
 	/**
 	 * The geolocation
 	 */
 	public GeoPoint geopoint;
+	
+	/**
+	 * Constructor
+	 */
+	public Address()
+	{
+		this.street = "";
+		this.city = "";
+		this.state = "";
+		this.zipcode = "";
+		this.phoneNumber = "";
+		this.radius = 0;
+		this.geopoint = new GeoPoint(0, 0);
+	}
+	
+	/**
+	 * Parcelable constructor
+	 * @param in the parcel
+	 */
+	public Address (Parcel in)
+	{
+		this.street = in.readString();
+		this.city = in.readString();
+		this.state = in.readString();
+		this.zipcode = in.readString();
+		this.phoneNumber = in.readString();
+		this.radius = in.readInt();
+		
+		int latitude = in.readInt();
+		int logitude = in.readInt();
+		this.geopoint = new GeoPoint(latitude, logitude);
+	}
 	
 	/**
 	 * Return the street address.
@@ -62,4 +97,36 @@ public class Address
 		output += this.geopoint.toString();
 		return output;
 	}
+	
+	@Override
+	public int describeContents() 
+	{
+		return 0;
+	}
+
+	@Override
+	public void writeToParcel(Parcel dest, int flags) 
+	{
+		dest.writeString(this.street);
+		dest.writeString(this.city);
+		dest.writeString(this.state);
+		dest.writeString(this.zipcode);
+		dest.writeString(this.phoneNumber);
+		dest.writeInt(this.radius);
+		dest.writeInt(this.geopoint.getLatitudeE6());
+		dest.writeInt(this.geopoint.getLongitudeE6());
+	}
+	
+	public static final Parcelable.Creator<Address> CREATOR = new Parcelable.Creator<Address>()
+	{
+		public Address createFromParcel(Parcel in) 
+		{
+            return new Address(in);
+        }
+
+        public Address[] newArray(int size) 
+        {
+            return new Address[size];
+        }
+	};
 }
--- app/src/edu/gatech/oftentimes2000/data/Profile.java
+++ app/src/edu/gatech/oftentimes2000/data/Profile.java
@@ -1,16 +1,19 @@
 package edu.gatech.oftentimes2000.data;
 
-public class Profile 
+import android.os.Parcel;
+import android.os.Parcelable;
+
+public class Profile implements Parcelable
 {
 	/**
 	 * The user name
 	 */
-	public String username = "";
+	public String username;
 	
 	/**
 	 * The name
 	 */
-	public String name = "";
+	public String name;
 	
 	/**
 	 * The address
@@ -20,15 +23,72 @@ public class Profile
 	/**
 	 * The website URL
 	 */
-	public String url = "";
+	public String url;
 	
 	/**
 	 * The email
 	 */
-	public String email = "";
+	public String email;
 	
 	/**
 	 * About user
 	 */
-	public String about = "";
+	public String about;
+
+	/**
+	 * Constructor
+	 */
+	public Profile()
+	{
+		this.username = "";
+		this.name = "";
+		this.address = new Address();
+		this.url = "";
+		this.email = "";
+		this.about = "";
+	}
+	
+	/**
+	 * Parcelable constructor
+	 * @param in the parcel
+	 */
+	public Profile(Parcel in)
+	{
+		this.username = in.readString();
+		this.name = in.readString();
+		this.address = in.readParcelable(Address.class.getClassLoader());
+		this.url = in.readString();
+		this.email = in.readString();
+		this.about = in.readString();
+	}
+	
+	@Override
+	public int describeContents() 
+	{
+		return 0;
+	}
+
+	@Override
+	public void writeToParcel(Parcel dest, int flags) 
+	{
+		dest.writeString(this.username);
+		dest.writeString(this.name);
+		dest.writeParcelable(this.address, 0);
+		dest.writeString(this.url);
+		dest.writeString(this.email);
+		dest.writeString(this.about);
+	}
+	
+	public static final Parcelable.Creator<Profile> CREATOR = new Parcelable.Creator<Profile>()
+	{
+		public Profile createFromParcel(Parcel in) 
+		{
+			return new Profile(in);
+		}
+
+		public Profile[] newArray(int size) 
+		{
+			return new Profile[size];
+		}
+	};
 }
