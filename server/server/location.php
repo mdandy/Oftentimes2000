32d88cef9adab589f11517208a3586e7b22a4350
Michael Dandy <mdandy1988@gmail.com>
2012-11-25 22:13:02 -0500

    Get announcement by location

--- server/server/location.php
+++ server/server/location.php
@@ -14,4 +14,22 @@ function ping_server($gcm_id, $latitude, $longitude)
 	return json_encode($res);
 }
 
+function get_announcement_by_location($latitude, $longitude, $radius=0)
+{
+	DAL::connect();
+
+	// TODO: geopoint calculation
+	$from_latitude = $latitude;
+	$to_latitude = $latitude;
+	$from_longitude = $longitude;
+	$to_longitude = $longitude;
+
+	$announcements = DAL::get_announcement_by_location($from_latitude, $to_latitude, $from_longitude, $to_longitude);
+	DAL::disconnect();
+
+	if ($announcements != NULL)
+		return json_encode(array ("res" => "TRUE", "data" => $announcements));
+	return json_encode(array ("res" => "FALSE"));
+}
+
 ?>
