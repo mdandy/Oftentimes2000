0723890c8cb6ab793fd910779d2578962dea60e4
Michael Dandy <mdandy1988@gmail.com>
2012-11-25 21:13:23 -0500

    Change name

--- server/server/location.php
+++ server/server/location.php
@@ -2,22 +2,16 @@
 
 require_once("dal.php");
 
-function get_announcement_by_location($gcm_id, $latitude, $longitude)
+function ping_server($gcm_id, $latitude, $longitude)
 {
 	DAL::connect();
-	
-	// TODO: geopoint calculation
-	$from_latitude = $latitude;
-	$to_latitude = $latitude;
-	$from_longitude = $longitude;
-	$to_longitude = $longitude;
-	
-	$announcements = DAL::get_announcement_by_location($from_latitude, $to_latitude, $from_longitude, $to_longitude);
+	$success = DAL::update_device_location($gcm_id, $latitude, $longitude);
 	DAL::disconnect();
 	
-	if ($announcements != NULL)
-		return json_encode(array ("res" => "TRUE", "id" => $gcm_id, "data" => $announcements));
-	return json_encode(array ("res" => "FALSE"));
+	$res = array ("res" => "FALSE");
+	if ($success)
+		$res = array ("res" => "TRUE");
+	return json_encode($res);
 }
 
 ?>
