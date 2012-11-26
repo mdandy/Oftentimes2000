ccc56c30a2faa6d3649020b29a6d542d83ac5ba8
Jonathan Edwards <jedwards163@gmail.com>
2012-11-26 16:50:34 -0500

    Added bounding box to get_announcement_by_location

--- server/server/location.php
+++ server/server/location.php
@@ -18,11 +18,28 @@ function get_announcement_by_location($latitude, $longitude, $radius=0)
 {
 	DAL::connect();
 
+  $r = convert_miles_to_degrees($radius);
+
+  $add = 1 / cos((float)($r * 2)); 
+
+  $r = (int)($r * 1000000);
+  
 	// TODO: geopoint calculation
-	$from_latitude = $latitude;
-	$to_latitude = $latitude;
-	$from_longitude = $longitude;
-	$to_longitude = $longitude;
+	$from_latitude = $latitude - $r;
+  $to_latitude = $latitude + $r;
+
+
+	$from_longitude = $longitude - ($add * ($r*2));
+  $to_longitude = $longitude + ($add * ($r*2));
+
+  echo $from_latitude;
+  echo "\n";
+  echo $to_latitude;
+  echo "\n";  
+  echo $from_longitude;
+  echo "\n";  
+  echo $to_longitude;
+  echo "\n";  
 
 	$announcements = DAL::get_announcement_by_location($from_latitude, $to_latitude, $from_longitude, $to_longitude);
 	DAL::disconnect();
@@ -30,6 +47,11 @@ function get_announcement_by_location($latitude, $longitude, $radius=0)
 	if ($announcements != NULL)
 		return json_encode(array ("res" => "TRUE", "data" => $announcements));
 	return json_encode(array ("res" => "FALSE"));
+}
+
+function convert_miles_to_degrees($radius) {
+  $r = $radius / 69.047;
+  return $r;
 }
 
 ?>
